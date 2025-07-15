a42a539888bd1d1c410c78120946981b7fb98f80
Juan1794 <jmurillo@aromadepot.com>
2025-07-15 11:04:12 -0400

    Agrego carpeta .streamlit y configuracion de tema

--- main.py
+++ main.py
@@ -2,27 +2,38 @@ import streamlit as st
 import base64
 
 def set_background():
-    image_files = ["AMBER1.jpg", "MORINGA.jpg"]
-    for img_path in image_files:
-        try:
-            with open(img_path, "rb") as img_file:
-                img_bytes = img_file.read()
-                encoded_img = base64.b64encode(img_bytes).decode()
-                st.markdown(f"""
-                    <style>
-                    .stApp {{
-                        background-image: url("data:image/jpg;base64,{encoded_img}");
-                        background-size: cover;
-                        background-repeat: no-repeat;
-                        background-position: center;
-                    }}
-                    </style>
-                """, unsafe_allow_html=True)
-                break
-        except FileNotFoundError:
-            continue
+    img_path = "AMBER1.jpg"
+
+    with open(img_path, "rb") as img_file:
+        img_bytes = img_file.read()
+        encoded_img = base64.b64encode(img_bytes).decode()
+
+    st.markdown(f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/jpg;base64,{encoded_img}");
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+        }}
+
+        /* CAMBIO FUERTE DEL COLOR DEL SIDEBAR USANDO 'aside' */
+        aside[data-testid="stSidebar"] {{
+            background-color: #3E2723 !important;
+            padding-top: 2rem;
+        }}
+
+        aside[data-testid="stSidebar"] * {{
+            color: white !important;
+        }}
+        </style>
+    """, unsafe_allow_html=True)
 
 set_background()
 
-st.title("Sistema Profesional - Aroma Depot")
-st.markdown("## Hoja WEBSITE2")
+st.markdown("""
+<div style='text-align: center; padding-top: 100px;'>
+    <h1 style='font-size: 50px; color: white;'>Aroma Depot</h1>
+    <h3 style='font-size: 28px; color: white;'>Automatic Pricing System</h3>
+</div>
+""", unsafe_allow_html=True)
